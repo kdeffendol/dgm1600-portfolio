f59c19509e3a4f4e7b27515f56a8a84a6cac2467
Kelsey Deffendol <kelsey@deffendol.org>
2018-10-23 21:54:04 -0600

    fixed js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import {films} from './assets/films.js'
 import {planets} from './assets/planets.js'
+//import {getRandomPlanet} from './assets/functions.js'
 
 let filmList = document.querySelector("#film-list");
 let planetList = document.querySelector("#planet-list")
@@ -10,4 +11,10 @@ films.forEach((film) => {
     filmList.appendChild(listItem)
     console.log(film.title)
 })
-
+function getRandomPlanet() {
+    var randomId =  Math.floor((Math.random()*planets.length))
+    let planet = document.getElementById("planet");
+    planet.textContent = planets[randomId].name;
+   // console.log(planet) testing
+}
+document.getElementById("mybutton").onclick = getRandomPlanet;
